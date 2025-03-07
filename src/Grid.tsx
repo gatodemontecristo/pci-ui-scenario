8014a804b0e7980ed1ea14ed64e76e0162924dd8
gatodemontecristo <erickdevicente2024@outlook.com>
2025-03-06 23:50:19 -0500

    Task #2: add sorting and filtering to grid for all columns

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,25 +1,110 @@
 import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
+import { ColDef, IRowNode } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+const fnComparator = (
+  valueA: string | undefined,
+  valueB: string | undefined,
+  nodeA: IRowNode<any>,
+  nodeB: IRowNode<any>,
+  isDescending: boolean
+) => {
+  if (!valueA) return isDescending ? -1 : 1;
+  if (!valueB) return isDescending ? 1 : -1;
+
+  const numA = parseFloat(valueA);
+  const numB = parseFloat(valueB);
+
+  return numA - numB;
+};
+
+const fnComparatorPha = (
+  valueA: string,
+  valueB: string,
+  nodeA: IRowNode<any>,
+  nodeB: IRowNode<any>,
+  isDescending: boolean
+) => {
+  if (valueA === "n/a") return isDescending ? -1 : 1;
+  if (valueB === "n/a") return isDescending ? 1 : -1;
+
+  return valueA.localeCompare(valueB, undefined, { sensitivity: "base" });
+};
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
+  {
+    field: "designation",
+    headerName: "Designation",
+    sortable: true,
+    filter: "agTextColumnFilter",
+  },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    sortable: true,
+    filter: "agTextColumnFilter",
+  },
+  {
+    field: "h_mag",
+    headerName: "H (mag)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "moid_au",
+    headerName: "MOID (au)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "q_au_1",
+    headerName: "q (au)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "q_au_2",
+    headerName: "Q (au)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "period_yr",
+    headerName: "Period (yr)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    sortable: true,
+    comparator: fnComparator,
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "pha",
+    headerName: "Potentially Hazardous",
+    sortable: true,
+    comparator: fnComparatorPha,
+    filter: "agTextColumnFilte",
+  },
+  {
+    field: "orbit_class",
+    headerName: "Orbit Class",
+    filter: "agTextColumnFilter",
+    enableRowGroup: true,
+  },
 ];
 
 const NeoGrid = (): JSX.Element => {
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+    <div className="ag-theme-alpine" style={{ height: 900, width: 2000 }}>
       <p
         style={{
           width: "100vw",
